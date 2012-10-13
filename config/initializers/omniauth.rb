4baf86f7aaf199d6a9e3932a5fbcc5e47b436366
Manoj Mishra <b0rn2c0d3@gmail.com>
2012-10-13 19:58:30 +0530

    added views to Ad model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
-require 'openid/store/filesystem'
+# require 'openid/store/filesystem'
 
-if Rails.env.development? || Rails.env.test?
-  OmniAuth.config.full_host = "http://127.0.0.1:3000"
-end
+# if Rails.env.development? || Rails.env.test?
+#   OmniAuth.config.full_host = "http://127.0.0.1:3000"
+# end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+# end
