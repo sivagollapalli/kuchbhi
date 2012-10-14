0870f4c1bed0363b692ad4908ae9519636ece279
Jiren Patel <jiren@joshsoftware.com>
2012-10-14 10:24:13 +0530

    Omniauth and Home page layout

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,15 @@
-require 'openid/store/filesystem'
+ENV['TWITTER_KEY'] = "N8Z9w84TXg1o65YLQKcnA"
+ENV['TWITTER_SECRET'] = "j811pXOnvWPWDl5kKrlRQgbDKZemFMDQPGahsOnTvU"
 
-if Rails.env.development? || Rails.env.test?
-  OmniAuth.config.full_host = "http://127.0.0.1:3000"
+if Rails.env.production?
+  ENV['FACEBOOK_KEY'] = '114907765330779'
+  ENV['FACEBOOK_SECRET'] = '8e777fe4731d16a1a16c50bd371b2674'
+else
+  ENV['FACEBOOK_KEY'] = '286456701455015'
+  ENV['FACEBOOK_SECRET'] = '04da1b8e9e25fa1b04ec9f482b878829'
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :twitter,  ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
